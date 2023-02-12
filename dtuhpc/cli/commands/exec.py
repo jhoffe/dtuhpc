c83c7b0a4db21cb7d2efc0ffe2a887f575e0f7f3
Jonas Hoffmann <s204071@student.dtu.dk>
2023-02-12 15:18:28 +0100

    chore(docs): add comments for commands

--- dtuhpc/cli/commands/exec.py
+++ dtuhpc/cli/commands/exec.py
@@ -7,8 +7,8 @@ from dtuhpc.cli.cli_config import CLIConfig
 @click.argument("command", nargs=1, type=str, required=True)
 @click.pass_obj
 def exec(config: CLIConfig, command: str):
-    config.load_config()
     """Executes COMMAND on the DTU HPC Server."""
+    config.load_config()
     conn = config.connection()
     conn.run(command)
 
